5db26c2413247c1ee361a2fa3d29571521493249
Keller <keller.martin@goodville.com>
2019-08-02 16:26:57 -0400

    more classes added, starting with constructors and some initial parsing

--- newParse.js
+++ newParse.js
@@ -6,32 +6,68 @@ var Attribute = /** @class */ (function () {
     return Attribute;
 }());
 var TagNode = /** @class */ (function () {
-    function TagNode() {
+    function TagNode(tagText) {
+        // Set openTag
+        this.openTag = tagText;
+        // find attributes
+        while (true) {
+            break;
+        }
+        // find tag name
+        var insideTag = tagText.slice(2, tagText.length - 1);
+        if (insideTag.split(" ").length > 1) {
+            this.type = insideTag.split(" ")[0];
+        }
+        else {
+            this.type = insideTag;
+        }
+        // set open tag --- UNFINISHED
     }
+    TagNode.prototype.getOpenTag = function () {
+        return this.openTag;
+    };
     return TagNode;
+}());
+var Lexer = /** @class */ (function () {
+    function Lexer(rawData) {
+        this.rawData = rawData;
+        this.language = this.findLanguage();
+    }
+    Lexer.prototype.getDescriptorTag = function () {
+        var descriptorText = this.rawData.match(/<!.+?>/)[0];
+        var descriptorNode = new TagNode(descriptorText);
+        return descriptorNode;
+    };
+    Lexer.prototype.getLanguage = function () {
+        return this.language;
+    };
+    Lexer.prototype.findLanguage = function () {
+        var openTag = this.getDescriptorTag().getOpenTag();
+        var lang = openTag.slice(2, openTag.length - 1);
+        if (lang.split(" ").length > 1) {
+            return lang.split(" ")[1];
+        }
+        else {
+            return null;
+        }
+    };
+    return Lexer;
 }());
 var MiniDOM = /** @class */ (function () {
     function MiniDOM(rawData, language) {
+        this.lex = new Lexer(rawData);
         if (language != undefined) {
             this.language = language;
         }
         else {
-            this.language = this.findOpeningDescriptor();
+            this.language = this.lex.getLanguage();
             // parse first node for language
             // if none found, try to guess language
         }
     }
-    MiniDOM.prototype.findOpeningDescriptor = function () {
-        return null;
+    MiniDOM.prototype.getLanguage = function () {
+        return this.language;
     };
-    MiniDOM.Lexer = /** @class */ (function () {
-        function class_1() {
-        }
-        class_1.prototype.getNode = function () {
-            return null;
-        };
-        return class_1;
-    }());
     return MiniDOM;
 }());
 fs.readFile("input.html", "utf8", function (err, data) {
@@ -41,5 +77,6 @@ function main(err, data) {
     if (err)
         throw err;
     var miniDOM = new MiniDOM(data);
+    console.log(miniDOM.getLanguage());
     return null;
 }
--- newParse.ts
+++ newParse.ts
@@ -1,5 +1,5 @@
-var fs = require("fs");
-var out : string;
+let fs = require("fs");
+let out : string;
 
 class Attribute {
     property: string;
@@ -10,34 +10,87 @@ class TagNode {
     type: string;
     attributes: Attribute[];
     openTag: string;
+    language: string;
+
+    constructor(tagText: string, language?: string){
+        // Set openTag
+        this.openTag = tagText;
+
+        // find attributes
+        while(true){
+            break;
+        }
+
+        // find tag name
+        let insideTag = tagText.slice(2, tagText.length - 1);
+        if(insideTag.split(" ").length > 1){
+            this.type = insideTag.split(" ")[0];
+        } else {
+            this.type = insideTag;
+        }
+
+        // find tag language
+        if(language != undefined){
+            this.language = language;
+        }
+    }
+
+    public getOpenTag(): string {
+        return this.openTag;
+    }
+}
+
+class Lexer {
+    private rawData: string;
+    private language: string;
+
+    constructor(rawData: string){
+        this.rawData = rawData;
+        this.language = this.findLanguage();
+    }
+
+    getDescriptorTag(): TagNode {
+        let descriptorText = this.rawData.match(/<!.+?>/)[0];
+
+        let descriptorNode = new TagNode(descriptorText);
+        return descriptorNode;
+    }
+
+    getLanguage(): string {
+        return this.language;
+    }
+
+    private findLanguage(): string {
+        let openTag = this.getDescriptorTag().getOpenTag();
+        let lang = openTag.slice(2, openTag.length - 1);
+
+        if(lang.split(" ").length > 1){
+            return lang.split(" ")[1];
+        } else {
+            return null;
+        }
+    }
 }
 
 class MiniDOM {
     nodes: TagNode[];
     language: string;
-    
-    private static Lexer = class {
-        rawData: string;
-
-        getNode(): TagNode {
-
-
-            return null;
-        }
-    }
+    lex: Lexer;
 
     constructor(rawData: string, language?: string){
+        this.lex = new Lexer(rawData);
+
         if(language != undefined){
             this.language = language;
         } else {
-            this.language = this.findOpeningDescriptor();
+            this.language = this.lex.getLanguage();
             // parse first node for language
             // if none found, try to guess language
         }
     }
 
-    findOpeningDescriptor() : string {
-        return null;
+    public getLanguage(): string {
+        return this.language;
     }
 }
 
@@ -48,7 +101,8 @@ fs.readFile("input.html", "utf8", function(err: any, data: string) {
 function main(err: any, data: string) : string{
     if(err) throw err;
 
-    var miniDOM = new MiniDOM(data);
+    let miniDOM = new MiniDOM(data);
+    console.log(miniDOM.getLanguage());
 
     return null;
 }
