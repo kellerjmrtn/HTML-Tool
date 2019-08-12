6d01527a299836a23d576711a5fd100da77dac07
Keller <keller.martin@goodville.com>
2019-08-12 16:32:22 -0400

    brainstorming and minor changes

--- src/ts/class/TagNode.js
+++ src/ts/class/TagNode.js
@@ -4,10 +4,17 @@ var Attribute_1 = require("./Attribute");
 var TagNode = /** @class */ (function () {
     function TagNode(tagText) {
         // Set openTag
-        this.openTag = tagText;
+        if (tagText[1] == "/") {
+            this.openTag = "";
+            this.closeTag = tagText;
+        }
+        else {
+            this.openTag = tagText;
+            this.closeTag = "";
+        }
         // find tag name
         this.type = this.findTagName();
-        this.currentIndex = 0;
+        TagNode.currentIndex = 0;
         // find attributes
         this.attributes = this.findAllAttributes();
     }
@@ -17,14 +24,14 @@ var TagNode = /** @class */ (function () {
     TagNode.prototype.findNextAttribute = function () {
         var tagString = this.getOpenTagText().trim();
         var len = tagString.length;
-        var attrStart = this.currentIndex;
+        var attrStart = TagNode.currentIndex;
         var valStart = attrStart;
         var inSingleQuotes = false;
         var inDoubleQuotes = false;
         var inAttr = false;
         var inValue = false;
         var prop = "";
-        for (var i = this.currentIndex; i < len; i++) {
+        for (var i = TagNode.currentIndex; i < len; i++) {
             var charAt = tagString.charAt(i);
             if (charAt == " ") {
                 if (inSingleQuotes == false && inDoubleQuotes == false) {
@@ -38,7 +45,7 @@ var TagNode = /** @class */ (function () {
                             if (nextEquals != null) {
                                 nextEquals = nextEqualsMatch[0];
                                 if (!nextEquals.includes("=")) {
-                                    this.currentIndex = i;
+                                    TagNode.currentIndex = i;
                                     return new Attribute_1.Attribute(tagString.slice(attrStart, i), "");
                                 }
                             }
@@ -58,7 +65,7 @@ var TagNode = /** @class */ (function () {
             }
             else if (charAt == '"' && inSingleQuotes == false) {
                 if (inValue && inDoubleQuotes) {
-                    this.currentIndex = i + 1;
+                    TagNode.currentIndex = i + 1;
                     return new Attribute_1.Attribute(prop.trim(), tagString.slice(valStart, i + 1));
                 }
                 else {
@@ -67,7 +74,7 @@ var TagNode = /** @class */ (function () {
             }
             else if (charAt == "'" && inDoubleQuotes == false) {
                 if (inValue && inSingleQuotes) {
-                    this.currentIndex = i + 1;
+                    TagNode.currentIndex = i + 1;
                     return new Attribute_1.Attribute(prop.trim(), tagString.slice(valStart, i + 1));
                 }
                 else {
@@ -75,7 +82,7 @@ var TagNode = /** @class */ (function () {
                 }
             }
             if (i == len - 1 && inAttr) {
-                this.currentIndex = i + 1;
+                TagNode.currentIndex = i + 1;
                 return new Attribute_1.Attribute(tagString.slice(attrStart, i + 1), "");
             }
         }
@@ -83,6 +90,7 @@ var TagNode = /** @class */ (function () {
     };
     TagNode.prototype.findAllAttributes = function () {
         var allAttr = [];
+        TagNode.currentIndex = 0;
         while (true) {
             var attr = this.findNextAttribute();
             if (attr != null) {
@@ -92,75 +100,6 @@ var TagNode = /** @class */ (function () {
                 break;
             }
         }
-        /*let inAttr = false;
-        let inValue = false;
-        let inSingleQuotes = false;
-        let inDoubleQuotes = false;
-        let attrStart = 0;
-        let valStart = 0;
-        let prop: string;
-
-        for(let i = 0; i < len; i++){
-            let charAt = tagString.charAt(i);
-
-            if(charAt == " " && inSingleQuotes == false && inDoubleQuotes == false){
-                if(inAttr){
-                    let nextEqualsMatch = tagString.slice(i).match(/(\s*?)[^(\s)]/);
-                    let nextEquals = "";
-                    if(nextEquals != null){
-                        nextEquals = nextEqualsMatch[0]
-                    }
-
-                    if(!nextEquals.includes("=")){
-                        allAttr.push(new Attribute(tagString.slice(attrStart, i), ""));
-                        inAttr = false;
-                    }
-                } else {
-                    attrStart = i + 1;
-                    inAttr = true;
-                }
-            } else if(charAt == "=" && inSingleQuotes == false && inDoubleQuotes == false && inAttr == true){
-                prop = tagString.slice(attrStart, i);
-                inAttr = false;
-                inValue = true;
-                valStart = i + 1;
-            } else if(charAt == "'"){
-                if(inSingleQuotes){
-                    allAttr.push(new Attribute(prop, tagString.slice(valStart, i + 1)));
-                    inValue = false;
-                }
-
-                if(inDoubleQuotes == false){
-                    inSingleQuotes = !inSingleQuotes;
-                }
-            } else if(charAt == '"'){
-                if(inDoubleQuotes){
-                    allAttr.push(new Attribute(prop, tagString.slice(valStart, i + 1)));
-                    inValue = false;
-                }
-
-                if(inSingleQuotes == false){
-                    inDoubleQuotes = !inDoubleQuotes;
-                }
-            } else if(i == len - 1 && inAttr == true){
-                allAttr.push(new Attribute(tagString.slice(attrStart, i + 1), ""));
-            }
-        }*/
-        /*if(tagString != null){
-            let attrArray = tagString.split(" ");
-
-            for(let i = 0; i < attrArray.length; i++){
-                if(attrArray[i] != this.type){
-                    let attr = attrArray[i].split("=");
-
-                    if(attr.length > 1){
-                        allAttr.push(new Attribute(attr[0], attr[1]));
-                    } else {
-                        allAttr.push(new Attribute(attr[0], ""));
-                    }
-                }
-            }
-        }*/
         return allAttr;
     };
     TagNode.prototype.getOpenTag = function () {
